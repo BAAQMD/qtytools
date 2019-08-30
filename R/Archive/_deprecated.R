d87f582b6dee6efc7b1937dfd4114c734c63768a
David Holstius <david.holstius@gmail.com>
2019-08-30 15:31:55 -0700

    housecleaning

---
+++ R/Archive/_deprecated.R
@@ -0,0 +1,33 @@
+#' @note Deprecated
+#'
+total_quantities_by_ <- function (input_data, grp_vars, qty_var = qty_var, unit_var = unit_var, digits = Inf, signif = Inf) {
+
+  .Deprecated("total_quantities_by")
+
+  if (missing(qty_var)) qty_var <- find_var(input_data, suffix = "_qty")
+  if (missing(unit_var)) unit_var <- find_var(input_data, suffix = "_unit")
+
+  unit_value <- unique(input_data[[unit_var]])
+  is_atomic <- function (x) (length(x) == 1)
+  if (!is_atomic(unit_value)) stop("Mixed units not yet supported")
+
+  # Curry the `total` function
+  agg <- partial(total, digits = digits, signif = signif)
+
+  # Drop the units column (temporarily)
+  pruned <- select(input_data, -dplyr::matches(unit_var))
+
+  # Group, aggregate, and ungroup
+  grouped <- group_by_at(pruned, grp_vars) # TODO: add `...`
+  totaled <- summarise_at(grouped, qty_var, funs(agg)) # TODO: add `na.rm`
+  ungrouped <- ungroup(totaled)
+
+  # Restore the units column
+  ungrouped[[unit_var]] <- unit_value
+
+  class(ungrouped) <-
+    class(input_data)
+
+  return(ungrouped)
+
+}
---
+++ R/Archive/defunct.R
@@ -0,0 +1,35 @@
+
+#' Total quantities by year
+#'
+#' @export
+annual_quantities_by0 <- function (input_data, ..., qty_var, unit_var, year_var = "year", digits = Inf, signif = Inf) {
+
+  .Defunct("annual_quantities_by")
+
+  grp_vars <- select_vars(names(input_data), ...)
+  grp_vars <- union(grp_vars, c(year = year_var))
+
+  totaled <-
+    total_quantities_by_(
+      input_data,
+      grp_vars = grp_vars,
+      qty_var = qty_var,
+      unit_var = unit_var,
+      digits = digits,
+      signif = signif)
+
+  # Always return `year` first
+  tidied <-
+    select(
+      totaled,
+      dplyr::one_of(year_var),
+      everything())
+
+  class(tidied) <-
+    union(
+      c("annual", "inventory"),
+      class(input_data))
+
+  return(tidied)
+
+}
