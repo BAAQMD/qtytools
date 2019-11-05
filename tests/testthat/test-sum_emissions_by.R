0854566a0fbef86cd2405337942ef8c3539617b2
David Holstius <david.holstius@gmail.com>
2019-11-05 13:28:23 -0800

    add tests for digits argument

--- tests/testthat/test-sum_emissions_by.R
+++ tests/testthat/test-sum_emissions_by.R
@@ -17,3 +17,32 @@ test_that("sum throughputs by pol_abbr", {
 
 })
 
+test_that("digits = -1", {
+
+  unrounded <-
+    ems_only %>%
+    sum_emissions_by(
+      pol_abbr)
+
+  rounded <-
+    ems_only %>%
+    sum_emissions_by(
+      pol_abbr,
+      digits = -1)
+
+  # should not be equal
+  expect_failure(
+    expect_equal(
+      unrounded,
+      rounded))
+
+  # manual rounding should generate the same result
+  unrounded %>%
+    mutate_at(
+      vars(ems_qty),
+      ~ base::round(., digits = -1)) %>%
+    expect_equal(
+      rounded)
+
+})
+
--- tests/testthat/test-sum_quantities_by.R
+++ tests/testthat/test-sum_quantities_by.R
@@ -43,3 +43,32 @@ test_that("signif = 2", {
 
 })
 
+test_that("digits = -1", {
+
+  unrounded <-
+    ems_only %>%
+    sum_quantities_by(
+      pol_abbr)
+
+  rounded <-
+    ems_only %>%
+    sum_quantities_by(
+      pol_abbr,
+      digits = -1)
+
+  # should not be equal
+  expect_failure(
+    expect_equal(
+      unrounded,
+      rounded))
+
+  # manual rounding should generate the same result
+  unrounded %>%
+    mutate_at(
+      vars(ems_qty),
+      ~ base::round(., digits = -1)) %>%
+    expect_equal(
+      rounded)
+
+})
+
