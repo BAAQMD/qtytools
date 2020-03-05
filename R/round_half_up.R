3f1a537c2183afa4ea61d4c49237fa45615e244d
David Holstius <david.holstius@gmail.com>
2020-03-05 14:43:03 -0800

    release

--- R/round_half_up.R
+++ R/round_half_up.R
@@ -2,8 +2,13 @@
 #'
 #' Round according to human-friendly "round-half-up" convention.
 #'
+#' @param x numeric
+#' @param digits integer
+#'
 #' @details
 #' R's `round()` follows the IEEE "round-to-even" rule, meaning that 1.5 will be rounded to 2, but 2.5 will also be rounded to 2. This isn't what many (most?) humans expect.
+#'
+#' @export
 round_half_up <- function (
   x,
   digits = 0
