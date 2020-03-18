2b6f5151599096d682c6b6ecdb5befc03829e9bd
David Holstius <david.holstius@gmail.com>
2020-03-18 11:04:34 -0700

    add tests

--- tests/testthat/test-round_half_up.R
+++ tests/testthat/test-round_half_up.R
@@ -17,3 +17,11 @@ test_that("0.15 rounds to 0.2", {
 test_that("0.25 rounds to 0.3", {
   expect_equal(round_half_up(0.25, digits = 1), 0.3)
 })
+
+test_that("pi (various digits)", {
+  expect_equal(round_half_up(pi, digits = 0), 3)
+  expect_equal(round_half_up(pi, digits = 1), 3.1)
+  expect_equal(round_half_up(pi, digits = 2), 3.14)
+  expect_equal(round_half_up(pi, digits = 3), 3.142)
+  expect_equal(round_half_up(pi, digits = 4), 3.1416)
+})
