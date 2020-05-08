79cd3c52c0767fb6bf42eeeb48bb06ddc6bb364c
David Holstius <david.holstius@gmail.com>
2020-05-08 14:23:06 -0700

    remove is_scalar()

--- R/is_scalar.R
+++
@@ -1,6 +0,0 @@
-#' Test whether a vector has exactly one element
-#'
-#' @importFrom assertive is_scalar
-#' @description imported from package `assertive`
-#' @export
-is_scalar <- assertive::is_scalar
