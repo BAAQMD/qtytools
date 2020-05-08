c4299ef51672f36599dfbde138cbdaaf64680152
David Holstius <david.holstius@gmail.com>
2020-05-08 14:52:57 -0700

    remove test for MMTCO2eq unit awareness

--- tests/testthat/test-MMTCO2eq.R
+++ tests/testthat/test-MMTCO2eq.R
@@ -2,12 +2,10 @@ context("MMTCO2eq")
 
 test_that("MMTCO2eq", {
 
-  library(units)
-
-  x <- as_units(1, "MMTCO2eq")
-
-  expect_equal(
-    set_units(x, "TCO2eq"),
-    as_units(1e6, "TCO2eq"))
+  # x <- units::as_units(1, "MMTCO2eq")
+  #
+  # expect_equal(
+  #   units::set_units(x, "TCO2eq"),
+  #   units::as_units(1e6, "TCO2eq"))
 
 })
