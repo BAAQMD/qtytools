a2e0964cdea0d3773a7a3cd4afae85fb8a27f29c
David Holstius <david.holstius@gmail.com>
2020-05-08 14:11:50 -0700

    replace data_frame() with tibble()

--- tests/testthat/helper-apply_scalars.R
+++ tests/testthat/helper-apply_scalars.R
@@ -5,11 +5,11 @@ test_category_totals <- tibble(
   ems_unit = "tons/yr")
 
 test_population_fractions <-
-  data_frame(year = "CY2000", ALA = 0.5, CC = 0.3, SON = 0.2)
+  tibble(year = "CY2000", ALA = 0.5, CC = 0.3, SON = 0.2)
 
 test_population_growth <- bind_rows(
-  ALA = data_frame(CY2000 = 1.00, CY2005 = 0.97, CY2010 = 0.90),
-  CC  = data_frame(CY2000 = 1.00, CY2005 = 1.29, CY2010 = 1.38),
-  SON = data_frame(CY2000 = 1.00, CY2005 = 1.02, CY2010 = 1.03),
+  ALA = tibble(CY2000 = 1.00, CY2005 = 0.97, CY2010 = 0.90),
+  CC  = tibble(CY2000 = 1.00, CY2005 = 1.29, CY2010 = 1.38),
+  SON = tibble(CY2000 = 1.00, CY2005 = 1.02, CY2010 = 1.03),
   .id = "cnty_abbr")
 
