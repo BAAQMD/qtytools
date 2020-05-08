ec514a0815796e640ce2b0405318eca803424475
David Holstius <david.holstius@gmail.com>
2020-05-08 14:17:24 -0700

    remove hardcoded sink(file = "/dev/null")

--- R/zzz.R
+++ R/zzz.R
@@ -1,31 +1,3 @@
 .onLoad <- function(libname, pkgname) {
 
-  try(
-    units::install_symbolic_unit(
-      name = "TCO2eq",
-      warn = FALSE, # warn if already installed
-      dimensionless = TRUE))
-
-  sink_error <- function (e) {
-    return(invisible(e))
-  }
-
-  sink(file = "/dev/null")
-
-  tryCatch(
-    units::install_conversion_constant(
-      from = "g.CO2",
-      to = "TCO2eq",
-      const = 1e6),
-    error = sink_error)
-
-  tryCatch(
-    units::install_conversion_constant(
-      from = "TCO2eq",
-      to = "MMTCO2eq",
-      const = 1e-6),
-    error = sink_error)
-
-  sink(file = NULL)
-
 }
